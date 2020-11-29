68580688f26e115d2355cf2ac38dc8f6f8e3511d
Steven Woodward <steveanthony999@gmail.com>
2020-11-28 22:33:14 -0800

    added testimonials section

--- scripts.js
+++ scripts.js
@@ -165,3 +165,25 @@ prints.forEach((print) => {
     ease: 'elastic',
   });
 });
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+});
